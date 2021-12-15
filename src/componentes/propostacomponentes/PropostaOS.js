d56d6cf267f1cfd53f83ae33d7327bf9df621f61
William Queiroz <wrequiao@gmail.com>
2021-12-15 19:37:16 -0300

    Tela de pop-up da OS

--- src/componentes/propostacomponentes/PropostaOS.js
+++ src/componentes/propostacomponentes/PropostaOS.js
@@ -1,67 +1,10 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import React, { Component, useState, useRef, useEffect, memo } from 'react';
+import { StyleSheet, View, ScrollView, Alert, Text, Button, Modal, Image, TouchableOpacity, Animated, } from 'react-native';
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default class PropostaOS extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['', 'Código', 'Núm', 'Data', 'Status', 'Tipo'],
-      widthArr: [30, 55, 55, 70, 100, 30],
-      tableData: [[]]
-    }
-  }
-
-  _alertIndex(index) {
-    Alert.alert(`This is row ${index}`);
-  }
-
-  render() {
-    const state = this.state;
-    const element = (data, index) => (
-      <TouchableOpacity onPress={() => this._alertIndex(data.OS_Codigo + ' - ' + data.TipoOS_Sigla)}>
-        <View style={styles.ico}>
-          <Icon name="search" color="#0000CD" />
-        </View>
-      </TouchableOpacity>
-    );
-    const data = [];
-    let contador=0;
-    this.state.tableData = this.props.OSs.map(os => {
-      data.push([element(os, contador), os.OS_Codigo, os.OS_Numero, moment(os.OS_DataAberturao).format('DD-MM-YY'), os.OS_Status, os.TipoOS_Sigla])
-      contador++;
-    })
-
-    return (
-      <View style={styles.container}>
-        <ScrollView horizontal={true}>
-          <View>
-            <Table borderStyle={{borderColor: '#C1C0B9'}}>
-              <Row widthArr={state.widthArr} data={state.tableHead} widthArr={state.widthArr} style={styles.head} textStyle={styles.text}/>
-            </Table>
-            <ScrollView style={styles.dataWrapper}>
-              <Table borderStyle={{borderColor: '#C1C0B9'}}>
-                {
-                    data.map((dataRow, index) => (
-                    <Row
-                      key={index}
-                      data={dataRow}
-                      widthArr={state.widthArr}
-                      style={[styles.row, index%2 && {backgroundColor: '#ffffff'}]}
-                      textStyle={styles.text}
-                    />
-                    ))
-                }
-              </Table>
-            </ScrollView>
-          </View>
-        </ScrollView>
-      </View>
-    )
-  }
-}
+const ModalPoup = ({osParam, visible, children}) => {
 
 const styles = StyleSheet.create({
   container: { 
@@ -89,4 +32,220 @@ const styles = StyleSheet.create({
    justifyContent: "center",
    alignItems: "center",
   },
+  modalBackGround: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '90%',
+    height: '90%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
 });
+  const [os, setOs] = useState(osParam);
+  const [showModal, setShowModal] = useState(visible);
+  const scaleValue = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    console.log('osparamff')
+    console.log(osParam)
+    toggleModal();
+  }, [visible]);
+  const toggleModal = () => {
+    if (visible) {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+  return (
+    <Modal transparent visible={showModal} osParam={osParam}> 
+      <View style={styles.modalBackGround}>
+        <Text>{osParam}</Text>
+        <Animated.View
+          style={[styles.modalContainer, {transform: [{scale: scaleValue}]}]}>
+          {children}
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+const PropostaOS = props => {
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    padding: 16, 
+    paddingTop: 30, 
+    backgroundColor: '#ffffff' 
+  },
+  head: { 
+    height: 50, 
+    backgroundColor: '#fff' 
+  },
+  text: { 
+    textAlign: 'center', 
+    fontWeight: '200' 
+  },
+  dataWrapper: { 
+    marginTop: -1 
+  },
+  row: { 
+    height: 40, 
+    backgroundColor: '#F7F8FA' 
+  },
+  ico: { 
+   justifyContent: "center",
+   alignItems: "center",
+  },
+  modalBackGround: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '90%',
+    height: '40%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+});
+
+const [visible, setVisible] = useState(false);
+
+const [viewModal, setviewModal] = useState(false);
+
+const [osData, setOsData] = useState({});
+
+  this.state = {
+      tableHead: ['', 'Código', 'Núm', 'Data', 'Status', 'Tipo'],
+      widthArr: [30, 55, 55, 70, 100, 30],
+      tableData: [[]],
+     // viewModal: false,    
+  }
+
+  const element = (data, index) => (
+    <TouchableOpacity onPress={() => this._alertIndex(data.OS_Codigo)}>
+      <View style={styles.ico}>
+        <Icon name="search" color="#0000CD" />
+      </View>
+    </TouchableOpacity>
+
+  );
+
+  const data = [];
+  let contador=0;
+  this.state.tableData = props.OSs.map(os => {
+    data.push([element(os, contador), os.OS_Codigo, os.OS_Numero, moment(os.OS_DataAberturao).format('DD-MM-YY'), os.OS_Status, os.TipoOS_Sigla])
+    contador++;
+  })
+
+  toggleModal = () => {
+    console.log('tooglew_visible')
+    console.log(visible)
+    //setviewModal(!viewModal)
+    setVisible(!visible)
+  // this.state.viewModal = !this.state.viewModal
+  };
+
+  _alertIndex = (os) => { 
+    //Alert.alert(`This is os ${JSON.stringify(os)}`);
+    //osParam = index;
+    setOsData(JSON.stringify(os));
+    this.toggleModal();
+  }
+
+  
+    return (
+      <View style={styles.container}>
+
+
+      <ModalPoup osParam={osData} visible={visible}>
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => setVisible(false)}>
+              <Image
+                source={require('~/assets/add.png')}
+                style={{height: 30, width: 30}}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{alignItems: 'center'}}>
+        <TouchableOpacity onPress={() => setVisible(false)}>
+          <Image
+            source={require('~/assets/avatar.png')}
+            style={{height: 150, width: 150, marginVertical: 10}}
+          />
+          </TouchableOpacity>
+        </View>
+
+        <Text style={{marginVertical: 30, fontSize: 20, textAlign: 'center'}}>
+          {osParam}
+        </Text>
+
+        <Text style={{marginVertical: 30, fontSize: 20, textAlign: 'center'}}>
+          Congratulations registration was successful
+        </Text>
+      </ModalPoup>
+
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderColor: '#C1C0B9'}}>
+              <Row widthArr={state.widthArr} data={state.tableHead} widthArr={state.widthArr} style={styles.head} textStyle={styles.text}/>
+            </Table>
+            <ScrollView style={styles.dataWrapper}>
+              <Table borderStyle={{borderColor: '#C1C0B9'}}>
+                {
+                    data.map((dataRow, index) => (
+                    <Row
+                      key={index}
+                      data={dataRow}
+                      widthArr={state.widthArr}
+                      style={[styles.row, index%2 && {backgroundColor: '#ffffff'}]}
+                      textStyle={styles.text}
+                    />
+                    ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
+    )
+  
+}
+
+export default memo(PropostaOS);
